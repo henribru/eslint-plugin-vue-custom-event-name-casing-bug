cd7439c0c9ba41005a7c6b966cafc4b776ee2367
henribru <6639509+henribru@users.noreply.github.com>
2024-10-15 13:50:50 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const emit = defineEmits({ 'foo-bar': () => true })
+
+emit('foo-bar')
+</script>
+
+<template>
+  <button @click="emit('foo-bar')">Foo</button>
+  <button @click="$emit('foo-bar')">Foo</button>
+</template>
